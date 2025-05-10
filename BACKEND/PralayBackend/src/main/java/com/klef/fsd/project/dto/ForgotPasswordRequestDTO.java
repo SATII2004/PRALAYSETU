59c79561d315afe25a81a8a4092839ffee4e64dc
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-05-10 08:04:25 +0530

    .

--- BACKEND/PralayBackend/src/main/java/com/klef/fsd/project/dto/ForgotPasswordRequestDTO.java
+++ BACKEND/PralayBackend/src/main/java/com/klef/fsd/project/dto/ForgotPasswordRequestDTO.java
@@ -1,5 +1,13 @@
 package com.klef.fsd.project.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class ForgotPasswordRequestDTO {
 
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    private String email;
 }
--- BACKEND/PralayBackend/src/main/java/com/klef/fsd/project/dto/UserResponseDTO.java
+++ BACKEND/PralayBackend/src/main/java/com/klef/fsd/project/dto/UserResponseDTO.java
@@ -1,5 +1,19 @@
 package com.klef.fsd.project.dto;
 
+import lombok.Data;
+
+@Data
 public class UserResponseDTO {
 
+    private Long id;
+    private String email;
+    private String role;
+    private String token;
+
+    public UserResponseDTO(Long id, String email, String role, String token) {
+        this.id = id;
+        this.email = email;
+        this.role = role;
+        this.token = token;
+    }
 }
